7f252c17e8b3d8e3f8e001e34d3887b00f646628
skraio <askar.r141@gmail.com>
2024-07-28 21:16:20 +0300

    Add trouble plugin

--- nvim/.config/nvim/lua/skraio/lazy/trouble.lua
+++ nvim/.config/nvim/lua/skraio/lazy/trouble.lua
@@ -1,37 +1,53 @@
+local keys = {
+    {
+        "<leader>ta",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+    },
+    {
+        "<leader>tc",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+        "]t",
+        function()
+            require("trouble").next({skip_groups=true, jump=true})
+        end,
+        desc = "Next Trouble Item",
+    },
+    {
+        "[t",
+        function()
+            require("trouble").prev({skip_groups=true, jump=true})
+        end,
+        desc = "Previous Trouble Item",
+    },
+    {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+    },
+    {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+    },
+    {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+    },
+}
+
 return {
     "folke/trouble.nvim",
     opts = {}, -- for default options, refer to the configuration section for custom setup.
     cmd = "Trouble",
-    keys = {
-        {
-            "<leader>xx",
-            "<cmd>Trouble diagnostics toggle<cr>",
-            desc = "Diagnostics (Trouble)",
-        },
-        {
-            "<leader>xX",
-            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-            desc = "Buffer Diagnostics (Trouble)",
-        },
-        {
-            "<leader>cs",
-            "<cmd>Trouble symbols toggle focus=false<cr>",
-            desc = "Symbols (Trouble)",
-        },
-        {
-            "<leader>cl",
-            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-            desc = "LSP Definitions / references / ... (Trouble)",
-        },
-        {
-            "<leader>xL",
-            "<cmd>Trouble loclist toggle<cr>",
-            desc = "Location List (Trouble)",
-        },
-        {
-            "<leader>xQ",
-            "<cmd>Trouble qflist toggle<cr>",
-            desc = "Quickfix List (Trouble)",
-        },
-    },
+    keys = keys
 }
