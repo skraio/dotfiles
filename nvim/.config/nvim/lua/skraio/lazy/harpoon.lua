a56e7bd967238c839548e139455e929465a21c09
skraio <askar.r141@gmail.com>
2024-08-19 19:04:41 +0300

    Update keyboard config

--- nvim/.config/nvim/lua/skraio/lazy/harpoon.lua
+++ nvim/.config/nvim/lua/skraio/lazy/harpoon.lua
@@ -10,14 +10,14 @@ return {
         vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
         vim.keymap.set("n", "<leader>he", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<leader>0", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<leader>1", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<leader>2", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<leader>3", function() harpoon:list():select(4) end)
 
-        vim.keymap.set("n", "<leader>h1", function() harpoon:list():replace_at(1) end)
-        vim.keymap.set("n", "<leader>h2", function() harpoon:list():replace_at(2) end)
-        vim.keymap.set("n", "<leader>h3", function() harpoon:list():replace_at(3) end)
-        vim.keymap.set("n", "<leader>h4", function() harpoon:list():replace_at(4) end)
+        vim.keymap.set("n", "<leader>h0", function() harpoon:list():replace_at(1) end)
+        vim.keymap.set("n", "<leader>h1", function() harpoon:list():replace_at(2) end)
+        vim.keymap.set("n", "<leader>h2", function() harpoon:list():replace_at(3) end)
+        vim.keymap.set("n", "<leader>h3", function() harpoon:list():replace_at(4) end)
     end
 }
