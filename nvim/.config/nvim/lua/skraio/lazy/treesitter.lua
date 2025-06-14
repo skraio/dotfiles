2f4e97edeb5fe25fc0a882935cf869e7524c8562
skraio <askar.r385@gmail.com>
2025-06-14 23:34:42 +0500

    upd

--- nvim/.config/nvim/lua/skraio/lazy/treesitter.lua
+++ nvim/.config/nvim/lua/skraio/lazy/treesitter.lua
@@ -6,7 +6,7 @@ return {
         config = function()
             require 'nvim-treesitter.configs'.setup {
                 -- -- A list of parser names, or "all" (the five listed parsers should always be installed)
-                ensure_installed = { "bash", "python", "go", "c", "lua", "vim", "vimdoc", "query", "xml", "http", "json", "graphql" },
+                -- ensure_installed = { "bash", "python", "go", "c", "lua", "vim", "vimdoc", "query", "xml", "http", "json", "graphql" },
 
                 -- -- Install parsers synchronously (only applied to `ensure_installed`)
                 sync_install = false,
@@ -24,7 +24,7 @@ return {
                 textobjects = {
                     move = {
                         enable = true,
-                        set_jumps = true,  -- whether to set jumps in the jumplist
+                        set_jumps = true, -- whether to set jumps in the jumplist
                         goto_next_start = {
                             ["]f"] = "@function.outer",
                         },
@@ -38,5 +38,27 @@ return {
     },
     {
         "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        config = function()
+            require 'treesitter-context'.setup {
+                enable = false,            -- Enable this plugin (Can be enabled/disabled later via commands)
+                multiwindow = false,      -- Enable multiwindow support.
+                max_lines = 0,            -- How many lines the window should span. Values <= 0 mean no limit.
+                min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+                line_numbers = true,
+                multiline_threshold = 20, -- Maximum number of lines to show for a single context
+                trim_scope = 'outer',     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+                mode = 'cursor',          -- Line used to calculate context. Choices: 'cursor', 'topline'
+                -- Separator between context and content. Should be a single character string, like '-'.
+                -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+                separator = nil,
+                zindex = 20,     -- The Z-index of the context window
+                on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+            }
+
+            vim.keymap.set("n", "<leader>tsc", "<Cmd>TSContext toggle<CR>", {})
+        end
     }
 }
